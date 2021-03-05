735d19c9a8bd4e638de4359495d9be137bb6894c
Tamoghna Kommaraju <tamoghnak13@gmail.com>
2021-03-05 10:46:05 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+client.login("secrets.SECRET_TOKEN");
+client.login("CODE.SECRET_TOKEN");
 client.login("secrets.CODE");
 client.on('ready', () => {
     console.log('Bot ready');
